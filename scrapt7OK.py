314433d21346b7a1fe97a18a1534a72fb3200585
fmonfasani <fmonfasani@gmail.com>
2024-11-15 08:29:19 -0300

    Srapping listo

--- scrapt7OK.py
+++ scrapt7OK.py
@@ -10,7 +10,7 @@ from datetime import datetime
 start_time = time.time()
 
 # Configuración de descarga y opciones de Chrome
-base_download_dir = os.path.join(os.getcwd(), "Descargas_PDFs")
+base_download_dir = os.path.join(os.getcwd(), "Descargas")
 if not os.path.exists(base_download_dir):
     os.makedirs(base_download_dir)
 
@@ -98,12 +98,97 @@ stock_accounts = [
     {"id": 358572, "text": "CUZZIOL GRACIELA RITA", "number": 343121},
     {"id": 360330, "text": "PUJALTE FACUNDO LUIS", "number": 344897},
     {"id": 360555, "text": "ZALAZAR RODRIGO EZEQUIEL ALBERTO", "number": 345125},
+    {"id": 361521, "text": "SILVA ARNALDO WILFRIDO", "number": 346093},
+    {"id": 367594, "text": "VALLEJOS DIEGO MARTIN", "number": 352185},
+    {"id": 369577, "text": "BALMACEDA JORGE BENJAMIN OSVALDO", "number": 354180},
+    {"id": 372518, "text": "SAENZ CARINA NOEMI", "number": 357146},
+    {"id": 372815, "text": "MENDIBURU JUAN JOSE", "number": 357445},
+    {"id": 374275, "text": "RADKE LETICIA LIZ", "number": 358914},
+    {"id": 378018, "text": "ALBES ANGEL OSCAR", "number": 362670},
+    {"id": 378659, "text": "SEGOVIA RODRIGO IVAN", "number": 363322},
+    {"id": 384054, "text": "TREPPO CATTANEO MONICA ALEJANDRA", "number": 368757},
+    {"id": 384181, "text": "RIOS EDUARDO", "number": 368884},
+    {"id": 386489, "text": "STECHINA DAHIANA ANELEY", "number": 371206},
+    {"id": 391940, "text": "PELIZARDI OSCAR EDUARDO", "number": 155463},
+    {"id": 392047, "text": "HAASE PABLO ANDRES", "number": 376801},
+    {"id": 392520, "text": "VELEZ OSPINA BAYRON AUGUSTO", "number": 377270},
+    {"id": 393118, "text": "VERON LILIAN ROSAURA", "number": 377866},
+    {"id": 395300, "text": "ALTAMIRANO BARRENECHEA LUCILA ROSALIA", "number": 380045},
+    {"id": 396517, "text": "DOMINGUEZ MACHADO MARIA ALEJANDRA", "number": 381259},
+    {"id": 396790, "text": "RUIZ DIAZ JUAN PABLO", "number": 381531},
+    {"id": 398239, "text": "ECHARRI MARIANA ALEJANDRA", "number": 382977},
+    {"id": 399504, "text": "OCENTINK ÁNGEL JUAN", "number": 384235},
+    {"id": 403657, "text": "ALVAREZ AUCAR SEBASTIAN GABRIEL", "number": 388372},
+    {"id": 406391, "text": "CABRERA ALEJANDRA INES", "number": 397777},
+    {"id": 409910, "text": "ALTA CALIDAD SRL", "number": 155538},
+    {"id": 414187, "text": "VEGA GUSTAVO FABIAN", "number": 155550},
+    {"id": 420354, "text": "GRIMBERG SANDRA CLARISSE", "number": 155580},
+    {"id": 423679, "text": "YANDA MABEL GLADYS", "number": 414873},
+    {"id": 428055, "text": "ARCARO WALTER ALEJANDRO", "number": 419247},
+    {"id": 428142, "text": "JOSE MARÍA DE LOS ANGELES", "number": 419334},
+    {"id": 433545, "text": "RODRIGUEZ VALLEJOS NOELIA AYMARA", "number": 900058},
+    {"id": 433546, "text": "ARDUÑA JOSE LUIS", "number": 900059},
+    {"id": 434359, "text": "ROLON LUCAS HERNAN", "number": 425545},
+    {"id": 440180, "text": "LEZCANO GLORIA MARIEL", "number": 431348},
+    {"id": 442848, "text": "BOGARIN MARTA EDITH", "number": 434023},
+    {"id": 443079, "text": "NUÑEZ JUAN MANUEL", "number": 434251},
+    {"id": 444696, "text": "RAMIREZ NORBERTO GABRIEL", "number": 435860},
+    {"id": 445003, "text": "FISCHER GUILLERMO CARLOS", "number": 436164},
+    {"id": 445099, "text": "VICENTE MARIA TERESA", "number": 436260},
+    {"id": 450382, "text": "ALEGRE NESTOR FABIAN", "number": 441524},
+    {"id": 451851, "text": "LINARES SILVIA DEL CARMEN", "number": 442983},
+    {"id": 452431, "text": "CREMADES MARCOS", "number": 443561},
+    {"id": 454579, "text": "MEZA MATIAS NICOLAS", "number": 900158},
+    {"id": 458354, "text": "ROMANO JUAN MANUEL", "number": 900186},
+    {"id": 459579, "text": "VELAZQUEZ ARIEL WALTER", "number": 450721},
+    {"id": 464514, "text": "GOMEZ JOSE HERNAN", "number": 455646},
+    {"id": 466267, "text": "GAITAN PEDRO LUIS", "number": 457392},
+    {"id": 469482, "text": "MAIDANA MARTIN ALEJANDRO", "number": 460564},
+    {"id": 472336, "text": "VEGA RAMON EUSEBIO", "number": 463402},
+    {"id": 472781, "text": "SOSA ARIEL ALEJANDRO", "number": 463837},
+    {"id": 472821, "text": "ROBLEDO DUARTE FATIMA VANINA", "number": 463877},
+    {"id": 474627, "text": "BERNARDIS CLAUDIA GABRIELA", "number": 465662},
+    {"id": 474950, "text": "RAMIREZ NOELIA ALEJANDRA", "number": 465979},
+    {"id": 476492, "text": "SALDAÑA MARÍA BELEN", "number": 467542},
+    {"id": 476994, "text": "MARTINEZ SOLIS EMANUEL MATIAS", "number": 468044},
+    {"id": 477132, "text": "RODRIGUEZ VALLEJOS LUCIA AYELEN", "number": 900339},
+    {"id": 480533, "text": "RAFFIN FERNANDO DANIEL", "number": 471785},
+    {"id": 481267, "text": "GÓMEZ MARIELA VICTORIA", "number": 472601},
+    {"id": 483156, "text": "LEZCANO JUAN MARCELO - Catalaxia SA", "number": 900422},
+    {"id": 486496, "text": "IRALA JUAN ALBERTO", "number": 478057},
+    {"id": 486658, "text": "PARED JULIO CESAR", "number": 478249},
+    {"id": 487173, "text": "GONZALEZ MARCELO ALEJANDRO", "number": 478823},
+    {"id": 488531, "text": "ALMIRON MARISOL ESTEFANIA", "number": 480314},
+    {"id": 491711, "text": "ESCOBAR LUIS ALBERTO", "number": 483757},
+    {"id": 493466, "text": "ZAMPA YLDA LUCIA", "number": 900516},
+    {"id": 493968, "text": "MAURICE GASTON FEDERICO", "number": 486107},
+    {"id": 494506, "text": "SUAREZ ANALIA VERONICA", "number": 486650},
+    {"id": 494519, "text": "PEREZ MARIA ISABEL", "number": 486663},
+    {"id": 494801, "text": "MARINICH ROSA HORTENCIA", "number": 486952},
+    {"id": 495426, "text": "MAURIÑO EDUARDO ALFREDO", "number": 487598},
+    {"id": 496511, "text": "QUIROZ FERRARI LUIS ERNESTO", "number": 488735},
+    {"id": 496514, "text": "SARTORI CARINA CECILIA", "number": 488738},
+    {"id": 497126, "text": "HUGO DAVID DELLAMEA", "number": 489348},
+    {"id": 499583, "text": "BENITEZ ROLLET SILVIA MELINA", "number": 900568},
+    {"id": 500839, "text": "VALDEZ MARIELA ALEJANDRA", "number": 493209},
+    {"id": 501920, "text": "SOSA PAULA LUCIA", "number": 494348},
+    {"id": 503460, "text": "MEDINA MARIA ELENA", "number": 495888},
+    {"id": 503623, "text": "BRITO FABIÁN ALEJANDRO", "number": 496054},
+    {"id": 505239, "text": "SALINAS ALEJANDRO FABIO", "number": 497682},
+    {"id": 505538, "text": "LEVATTI JORGE MARCELO", "number": 497967},
+    {"id": 507247, "text": "RADZANOWICZ FEDERICO", "number": 499649},
+    {"id": 507334, "text": "AVILA OMAR ENRIQUE", "number": 499736},
+    {"id": 507836, "text": "MARTINEZ SOLIS EMANUEL MATIAS", "number": 900655},
+    {"id": 508939, "text": "ROMERO PABLO LUIS", "number": 501213},
+    {"id": 509021, "text": "VERNAL MARIANA ITATI", "number": 501291},
+    {"id": 513662, "text": "PANIAGUA CARLOS ALBERTO", "number": 505840},
+    {"id": 515329, "text": "PARED ALFREDO JAVIER", "number": 507466}
 ]
 
 # Fechas para el reporte
-fecha_inicio = "01/01/2024"
+fecha_inicio = "01/10/2024"
 fecha_fin = "31/10/2024"
-fecha_nombre = "Reporte_2024"  # para usar en el nombre del archivo
+fecha_nombre = "Reporte_Octubre_2024"  # para usar en el nombre del archivo
 
 # Función para esperar hasta que el archivo esté completamente disponible
 def esperar_descarga_completa(nombre_base, tiempo_espera=120):
